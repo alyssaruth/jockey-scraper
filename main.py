69c7961f512f412ce54f5f59ae2b50feb52075cb
Alex <alex.burlton@sonocent.com>
2021-03-17 08:32:05 +0000

    rm redunant escape chars

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ BASE_URL = 'https://no-more-jockeys.fandom.com'
 def get_game_pages() -> Set[str]:
     result = requests.get(f'{BASE_URL}/wiki/Category:Game').text
 
-    matches: List[str] = re.findall(r'(<a href="\/wiki\/.*" class="category-page__member-link" title=".*">)', result)
+    matches: List[str] = re.findall(r'(<a href="/wiki/.*" class="category-page__member-link" title=".*">)', result)
     all_pages: Set[str] = set()
     for match in matches:
         page_slug = re.search(r'href="(.*)" class', match).group(1)
