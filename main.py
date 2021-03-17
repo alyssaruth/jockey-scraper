afa40a9083d39542f510630c1ad61847a5f20aec
Alex <alex.burlton@sonocent.com>
2021-03-17 08:32:32 +0000

    rm pycharm comment

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def get_game_pages() -> Set[str]:
     return all_pages
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     pages = get_game_pages()
     print(pages)
