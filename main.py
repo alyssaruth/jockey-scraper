8840966a8eb18c374456dfbd1e6d906e8459d489
Alex <alex.burlton@sonocent.com>
2021-03-17 08:31:03 +0000

    extract BASE_URL

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from typing import Set, List
 import requests
 import re
 
+BASE_URL = 'https://no-more-jockeys.fandom.com'
+
 
 def get_game_pages() -> Set[str]:
-    result = requests.get('https://no-more-jockeys.fandom.com/wiki/Category:Game').text
+    result = requests.get(f'{BASE_URL}/wiki/Category:Game').text
 
     matches: List[str] = re.findall(r'(<a href="\/wiki\/.*" class="category-page__member-link" title=".*">)', result)
     all_pages: Set[str] = set()
